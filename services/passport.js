02e21a0551fe478a72f397107116530b1b6cf5fa
Tri Tran <triit95@gmail.com>
2018-12-21 08:29:07 +0700

    connect to mongo

--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,35 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const KEYS = require('../config/keys');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
-//Client ID: 270741756236-1gm7o6tg705ispfdvjm5avvqsld24c0h.apps.googleusercontent.com
-//Client Serect: hsCA_qsjf_ehCTUCuNzCOGXW
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+//Client ID: 347723253198-uvl8dkvg0vrj0iv60a8tn66e06lucrh5.apps.googleusercontent.com
+//Client Serect: U7ssSq92WfExh6YhxxAUcHf_
 passport.use(
     new GoogleStrategy({
-      clientID: KEYS.googleClientID,
-      clientSecret: KEYS.googleClientSecret,
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access Token', accessToken);
-      console.log('refresh Token', refreshToken);
-      console.log('profile: ', profile);
+    //   console.log('access Token', accessToken);
+    //   console.log('refresh Token', refreshToken);
+    //   console.log('profile: ', profile);
+        User.findOne({ googleId: profile.id }).then((existingUser) => {
+                if (existingUser) {
+                    //we allready have a record with the given profile ID
+                    done(null, existingUser);
+                } else {
+                    //we don't have a user record with this ID, make a new record!
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            })
     })
   );
